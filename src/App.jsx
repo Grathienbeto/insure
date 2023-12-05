d90297b5ef2b41b5595c591b3deff1e1d03ad24c
Norberto Andres Luna <betoluna89@gmail.com>
2023-12-05 11:54:17 -0300

    Navbar mobile done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Navbar } from "./components/navbar/Navbar";
+
+// CSS
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,56 @@
-import React from "react";
+import "./navbar.css";
 
 export const Navbar = () => {
-  return <></>;
+  return (
+    <nav className="navbar navbar-expand-lg d-flex align-items-cente">
+      <div className="container-fluid mx-auto w-75">
+        <a className="navbar-brand m-3" href="#">
+          <img
+            src="https://raw.githubusercontent.com/Grathienbeto/insure/b5d6f3d4b7da4de0df98fe5f549a9cc28916b729/challenge/images/logo.svg"
+            alt="Insure Logo"
+          />
+        </a>
+
+        <button
+          className="navbar-toggler m-3 collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          id="menuBtn"
+        >
+          <span className="toggle-icon"></span>
+          <span className="toggle-icon-hidden-y"></span>
+          <span className="toggle-icon-hidden-x"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-end">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                HOW WE WORK
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                BLOG
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                ACCOUNT
+              </a>
+            </li>
+            <li className="nav-item" id="plans">
+              <a className="nav-link" href="#">
+                VIEW PLANS
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
