f677b7a3dedec185b933f062396bb88ce91caa8e
Norberto Andres Luna <betoluna89@gmail.com>
2023-12-05 16:17:38 -0300

    Ultimos toques al Navbar.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import "./navbar.css";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg d-flex align-items-cente">
+    <nav className="navbar navbar-expand-lg d-flex align-items-center p-0">
       <div className="container-fluid mx-auto w-75">
-        <a className="navbar-brand m-3" href="#">
+        <a className="navbar-brand m-4" href="#">
           <img
             src="https://raw.githubusercontent.com/Grathienbeto/insure/b5d6f3d4b7da4de0df98fe5f549a9cc28916b729/challenge/images/logo.svg"
             alt="Insure Logo"
@@ -12,7 +12,7 @@ export const Navbar = () => {
         </a>
 
         <button
-          className="navbar-toggler m-3 collapsed"
+          className="navbar-toggler m-4 collapsed"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -27,7 +27,7 @@ export const Navbar = () => {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-end">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-end align-items-end">
             <li className="nav-item">
               <a className="nav-link" href="#">
                 HOW WE WORK
