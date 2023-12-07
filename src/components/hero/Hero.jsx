98a782d6fa049e3068abeeb4b3555f24272cfbaf
Norberto Andres Luna <betoluna89@gmail.com>
2023-12-06 21:11:31 -0300

    Mobile hero done

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -18,9 +18,16 @@ export const Hero = () => {
       {/*  */}
 
       <div className="w-100" id="hero_wrapper">
-        <div className="hero_wrapper_container mx-auto w-75 d-flex">
+        <div className="hero_wrapper_container mx-auto d-flex">
           <hr className="little-line " />
+
           <div id="hero_text" className="">
+            <img
+              src="https://raw.githubusercontent.com/Grathienbeto/insure/f5020b1153eb6e21260a55d5d8c5d98e16bb644c/challenge/images/bg-pattern-intro-left-mobile.svg"
+              alt=""
+              className="mobile-bg-left hero-img-back-mobile"
+            />
+
             <h1>Humanizing your insurance.</h1>
             <p className="mb-5">
               Get your life insurance coverage easier and faster. We blend our
@@ -31,15 +38,28 @@ export const Hero = () => {
               VIEW PLANS
             </a>
           </div>
+
           <div id="hero_img">
-            <img
-              src="https://raw.githubusercontent.com/Grathienbeto/insure/main/challenge/images/image-intro-desktop.jpg"
-              alt=""
-              className=""
-            />
+            <picture>
+              <source
+                srcSet="https://raw.githubusercontent.com/Grathienbeto/insure/main/challenge/images/image-intro-mobile.jpg"
+                media="(orientation: portrait)"
+              />
+              <img
+                src="https://raw.githubusercontent.com/Grathienbeto/insure/main/challenge/images/image-intro-desktop.jpg"
+                alt=""
+              />
+            </picture>
           </div>
         </div>
+        <img
+          src="https://raw.githubusercontent.com/Grathienbeto/insure/f5020b1153eb6e21260a55d5d8c5d98e16bb644c/challenge/images/bg-pattern-intro-right-mobile.svg"
+          alt=""
+          className="mobile-bg-right hero-img-back-mobile"
+        />
       </div>
+
+      <div className="div-bot"></div>
     </section>
   );
 };
