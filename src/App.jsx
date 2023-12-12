25fa30d223242afb68278df2b7ff2294abf84696
Norberto Andres Luna <betoluna89@gmail.com>
2023-12-12 16:55:19 -0300

    Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Navbar } from "./components/navbar/Navbar";
 import { Hero } from "./components/hero/Hero";
 import { Standouts } from "./components/standouts/Standouts";
 import { Cta } from "./components/cta/Cta";
+import { Footer } from "./components/footer/Footer";
 
 // CSS
 import "./App.css";
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <Standouts />
       <Cta />
+      <Footer />
     </>
   );
 }
